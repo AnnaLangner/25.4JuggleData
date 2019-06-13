91c0fab449d8986580559016ae6941e24f2a7271
Ania <annalangner06@gmail.com>
2019-06-13 11:52:09 +0200

    the last attempt to run an app

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 bodyParser = require('body-parser');
 var app = express();
-app.use(express.static(`${__dirname}/assets`));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    console.log('takiej ścieżki będzie szukał Express: ',  __dirname + '/assets/index.html')
     res.sendFile(__dirname + '/assets/index.html');
-
 });
 
 app.get('/userform', function (req, res) {
